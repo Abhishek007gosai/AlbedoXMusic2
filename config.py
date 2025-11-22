cd15141eca235ce639941b60894a425af84b833c
Abhishek007gosai <99321716+Abhishek007gosai@users.noreply.github.com>
2025-11-22 16:23:31 +0530

    Update config.py
    Signed-off-by: Abhishek007gosai <99321716+Abhishek007gosai@users.noreply.github.com>

--- config.py
+++ config.py
@@ -5,36 +5,36 @@ load_dotenv()
 
 class Config:
     def __init__(self):
-        self.API_ID = int(getenv("API_ID", 0))
-        self.API_HASH = getenv("API_HASH")
+        self.API_ID = 23537462
+        self.API_HASH = "c9599a5aa61ee8ca4f5e778d20c61f24"
 
-        self.BOT_TOKEN = getenv("BOT_TOKEN")
-        self.MONGO_URL = getenv("MONGO_URL")
+        self.BOT_TOKEN = "BOT_TOKEN")
+        self.MONGO_URL = ("MONGO_URL")
 
-        self.LOGGER_ID = int(getenv("LOGGER_ID", 0))
-        self.OWNER_ID = int(getenv("OWNER_ID", 0))
+        self.LOGGER_ID = (-1002456565415)
+        self.OWNER_ID = 7654385403
 
         self.DURATION_LIMIT = int(getenv("DURATION_LIMIT", 60)) * 60
         self.QUEUE_LIMIT = int(getenv("QUEUE_LIMIT", 20))
         self.PLAYLIST_LIMIT = int(getenv("PLAYLIST_LIMIT", 20))
 
-        self.SESSION1 = getenv("SESSION", None)
+        self.SESSION1 = "BQG-QCUAokEaPu4bb4O9P59dbo9te5Gyc0K0Cqkrh--op5pGZgiLYqhsTG6Y1_N84u7SCErMr3EPBV184hL1TQOfl6_eH5WGSYokUegnSdGPsKRyBrNNwOalEkbMg695CwI_fquyzpSCg-ScBcNd40OOfI2REmmujpc7z6mzlt2cs83Wd1pfY-hHd3IzxW8g8ZZFIrxUT1A_vHDX7rRz3B2IsZ3dUJUAKRbHfac94IVQ4bZJZHKVYac_TIlReRqkQafDr5sf8wz9ge8NyvSzt5AkAiNeFK1lnParhNdZXZz8ahEE1h-qoArBrMvk449MZBdqYywSUc6iAfkizY8G0_WElIWtlgAAAAFZG6IeAA"
         self.SESSION2 = getenv("SESSION2", None)
         self.SESSION3 = getenv("SESSION3", None)
 
-        self.SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/FallenAssociation")
-        self.SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/DevilsHeavenMF")
+        self.SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/AnimeNexusNetwork/160")
+        self.SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/EternalsHelplineBot")
 
-        self.AUTO_END: bool = getenv("AUTO_END", False)
-        self.AUTO_LEAVE: bool = getenv("AUTO_LEAVE", False)
+        self.AUTO_END: bool = getenv("AUTO_END", True)
+        self.AUTO_LEAVE: bool = getenv("AUTO_LEAVE", True)
         self.VIDEO_PLAY: bool = getenv("VIDEO_PLAY", True)
         self.COOKIES_URL = [
-            url for url in getenv("COOKIES_URL", "").split(" ")
+            url for url in getenv("COOKIES_URL", "https://batbin.me/bractea").split(" ")
             if url and "batbin.me" in url
         ]
-        self.DEFAULT_THUMB = getenv("DEFAULT_THUMB", "https://te.legra.ph/file/3e40a408286d4eda24191.jpg")
-        self.PING_IMG = getenv("PING_IMG", "https://files.catbox.moe/haagg2.png")
-        self.START_IMG = getenv("START_IMG", "https://files.catbox.moe/zvziwk.jpg")
+        self.DEFAULT_THUMB = getenv("DEFAULT_THUMB", "https://files.catbox.moe/da1v00.jpg")
+        self.PING_IMG = getenv("PING_IMG", "https://files.catbox.moe/da1v00.jpg")
+        self.START_IMG = getenv("START_IMG", "https://files.catbox.moe/da1v00.jpg")
 
     def check(self):
         missing = [
